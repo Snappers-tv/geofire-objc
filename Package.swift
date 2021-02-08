37fb97027127a18337cc7871359b0b592a39223a
Alon Genosar <alon@bootleg.co.il>
2021-02-08 06:54:21 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
     ],
   
     targets: [
-        .target(name: "GeoFire",  dependencies: [], path: "")
+        .target(name: "GeoFire", dependencies: [], path: "./Sources/Soto/Services/ACM"),
     ]
 )
